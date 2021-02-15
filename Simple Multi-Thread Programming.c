bd2d01c27726fffe2047c3301e6d3e1e0e2b961f
KingdomCross <alexchheng1998@gmail.com>
2021-02-14 16:10:27 -0800

    Simple Thread template

--- Simple Multi-Thread Programming.c
+++ Simple Multi-Thread Programming.c
@@ -7,4 +7,14 @@ int SharedVariable = 0;
 void SimpleThread(int which){
     int num, val;
     
+    for(num = 0; num < 20; num++){
+        if (random() > RAND_MAX/2)
+        usleep(500);
+
+    val = SharedVariable;
+    printf("*** thread %d sees value %d\n", which, val);
+    SharedVariable = val + 1;
+    }
+    val = SharedVariable;
+    printf("*** thread %d sees final value %d\n", which, val);
 }
