2ac5595ec6fc0f8ae9150969089b946c4a75be4d
KingdomCross <alexchheng1998@gmail.com>
2021-02-20 16:05:42 -0800

    Rename file and continue working

--- mthread.c
+++ mthread.c
@@ -3,11 +3,29 @@ Alex Chheng
 Steven Barberi
 */
 
-//#include <pthread.h>
+#include <pthread.h>
 #include <stdio.h>
-//#include <unistd.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdint.h>
+//#include <stdint.h>
+/*
+void *func(void *var){
+    sleep(2);
+    printf("We are using thread \n");
+    return NULL;
+}
+
+int main(){
+    pthread_t t_id;
+    printf("Before using threading \n");
+    pthread_create(&t_id, NULL, func, NULL);
+    pthread_join(t_id, NULL);
+
+    printf("After thread \n");
+
+    exit(0);
+}
+*/
 
 int SharedVariable = 0;
 
