574597b21cad90e0d8f5a122a652cab746df1416
Steven <48588764+Steventbarberi@users.noreply.github.com>
2021-02-20 21:48:45 -0800

    Update mthread.c

--- mthread.c
+++ mthread.c
@@ -2,80 +2,81 @@
 Alex Chheng
 Steven Barberi
 */
-
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdint.h>
-int SharedVariable = 0;
+
+int sharedVariable = 0;
 pthread_mutex_t lock;
 pthread_barrier_t barrier;
 
 void SimpleThread(int which){
-    int num, val;
-    
-    for(num = 0; num < 20; num++){
-        if (random() > RAND_MAX/2){
-        usleep(500);
-        }
-    #ifdef PTHREAD_SYNC
-    	pthread_mutex_lock(&lock);
-    #endif
-    	printf("*** thread %d sees value %d\n", which, val);
-    	SharedVariable = val + 1;
-    #ifdef PTHREAD_SYNC
-    	pthread_mutex_unlock(&lock);
-    #endif
-    }
-    #ifdef PTHREAD_SYNC
-    	pthread_barrier_wait(&barrier);
-    #endif
-    val = SharedVariable;
-    printf("*** thread %d sees final value %d\n", which, val);
+	int num, val;
+	for(num = 0; num < 20; num++){
+		if(random() > RAND_MAX/2){
+			usleep(500);
+		}
+	
+	#ifdef PTHREAD_SYNC
+		pthread_mutex_lock(&lock);
+	#endif
+		val = sharedVariable;
+		printf("***thread%d sees value %d\n", which,val);
+		sharedVariable= val + 1;
+	#ifdef PTHREAD_SYNC
+		//unlocks to let the next thread go in
+		pthread_mutex_unlock(&lock);
+	#endif
+	}
+	#ifdef PTHREAD_SYNC
+		pthread_barrier_wait(&barrier);
+	#endif
+	val = sharedVariable;
+	printf("Thread%d sees final value %d\n", which, val);
 }
 
-void *ThreadID(void *arg){
-    int thread_id = (long) arg;
-    SimpleThread(thread_id);
-}
-
-void *func(void *var){
-    sleep(2);
-    printf("We are using thread \n");
-    return NULL;
+void *ThreadID(void * arg){
+	int thread_id =(long) arg;//type casted 
+	SimpleThread(thread_id);
 }
 
 int main(int argc, char *argv[]){
-    int i;
-    int thread_count;
-    if (argc != 2){
-        printf("Error: Enter one parameter only for the command line");
-        return(0);
-    }
-    else{
-        thread_count = atoi(argv[1]);
-        if(thread_count < 1){
-            printf("Enter whole number 1 or higher");
-            return 0;
-        }
-       
-        if(pthread_barrier_init(&barrier, NULL, thread_count)){
-            printf("Error: Barrier not created");
-            return -1;
-        }
-        
-        pthread_t array[thread_count];
+	int i;
+	int thread_count;
+	
+	if(argc != 2){
+		printf("Please enter only one parameter for the command line");
+		return(0);
+	}
+	else{
+		thread_count = atoi(argv[1]);
+		if(thread_count < 1){
+			printf("Enter a positive whole number...");
+			return 0;
+		}
+		
+		if(pthread_barrier_init(&barrier, NULL, thread_count)){
+			printf("Barrier not created...");
+			return -1;
+		}
 
-        for(i = 0; i < thread_count; i++){
-            pthread_create(&array[i], NULL, ThreadID, (void *)(long) i);
-        }
+		pthread_t array[thread_count];
 
-        for(i = 0; i < thread_count; i++){
-            pthread_join(array[i], NULL);
-        }
-    }
-    return 0;
+		for(i = 0; i < thread_count; i++){
+			pthread_create(&array[i], NULL, ThreadID,(void *)(long) i);
+		}
+		for ( i = 0; i < thread_count; i++){
+			pthread_join(array[i], NULL);
+		}
+		
+		
+	}
+	
+	return 0;
+}
+
     /*
     pthread_t t_id;
     printf("Before using threading \n");
