835be86c2b642d9da1b843257d1038cf7e5fca82
Steven <48588764+Steventbarberi@users.noreply.github.com>
2021-02-21 21:14:09 -0800

    Update mthread.c

--- mthread.c
+++ mthread.c
@@ -12,7 +12,7 @@ Steven Barberi
 #include <unistd.h>
 
 int sharedVariable = 0; // Initialize variable
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // Initialize the mutex lock
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // Initialize the mutex lock which locks the critical section
 pthread_barrier_t barrier; // Create a variable to block the final variable from showing early
 
 void SimpleThread(int which){
@@ -23,28 +23,29 @@ void SimpleThread(int which){
 		}
 	
 	#ifdef PTHREAD_SYNC // Use in a command line to call a mutex lock
-		//locks other threads from entering at the same time as the first one is being implemented
 		pthread_mutex_lock(&lock);
+		//locking the critical section
 	#endif
 		val = sharedVariable;
 		printf("***thread%d sees value %d\n", which,val);
 		sharedVariable= val + 1;
 	#ifdef PTHREAD_SYNC // The exit of the lock
-		//unlocks to let the next thread go in
 		pthread_mutex_unlock(&lock);
+		//unlocking critical section
+		//Will now allow another thread to execute
 	#endif
 	}
-	#ifdef PTHREAD_SYNC // Also included in a command line to call a barrier from showing the threads early
-		//syncs other threads together
-		//waits till all threads are here and releases them
+	#ifdef PTHREAD_SYNC 
+	// Also included in a command line to call a barrier from showing the threads early
 		pthread_barrier_wait(&barrier);
 	#endif
 	val = sharedVariable; // Make sharedVariable be the same as val
-	printf("Thread%d sees final value %d\n", which, val); // The final result of the thread which should show at the end
+	printf("Thread%d sees final value %d\n", which, val);
+	// The final result of the thread which should show at the end
 }
 
 void *ThreadID(void * argThread){
-	int thread_id = (long) argThread;//type casted 
+	int thread_id = (long) argThread;
 	SimpleThread(thread_id); // Call function with type casted
 }
 
@@ -52,8 +53,8 @@ int main(int argCommand, char *argInput[]){ // Take the command line and user st
 
 	int threadNumber = atoi(argInput[1]);//converts string to int
 	
-	if(argCommand != 2){//checks to see if only one parameter was taken
-		printf("Error: Enter only one parameter in the command line"); // Notify user of error
+	if(argCommand != 2){//Parameter checking
+		printf("Error: Please enter only one parameter."); // Notify user of error
 		return(0);
 	}
 	
@@ -62,19 +63,19 @@ int main(int argCommand, char *argInput[]){ // Take the command line and user st
 		return 0;
 	}
 	
-	if(pthread_barrier_init(&barrier, NULL, threadNumber)){//initialized to check if any thread is blocked
+	if(pthread_barrier_init(&barrier, NULL, threadNumber)){
 		printf("Error: Barrier is not created"); // Notify user of pthread error with barrier
 		return -1;
 	}
 
-	pthread_t pthreadArray[threadNumber];//creating a unique data type for threads
+	pthread_t pthreadArray[threadNumber];
 
 	for(int i = 0; i < threadNumber; i++){ // loop for number of thread
 		pthread_create(&pthreadArray[i], NULL, ThreadID,(void *)(long) i);//creates the threads
 	}
 
 	for (int i = 0; i < threadNumber; i++){ // loop for number of thread
-		pthread_join(pthreadArray[i], NULL);//waits for current thread to finish before calling the next one
+		pthread_join(pthreadArray[i], NULL);
 	}
 	
 	return 0;
